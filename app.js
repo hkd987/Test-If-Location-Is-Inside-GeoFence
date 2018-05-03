69e9423cfc77c54aa08b4a9c6ebaefc71e4e7e34
Lundin Matthews <lundinmatthews@me.com>
2018-05-02 20:50:33 -0500

    Proved use
    This function is used to determine if a point on a map is inside a known location.

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-const LOCATION_CHECK = (data) => {
+// NOTE: This would come from your database
+const models = () => {
+  return {
+    name: 'TEST',
+    latitude: 30.0000,
+    longitude: -95.00000,
+    radius: 175
+  }
+}
+
+const LOCATION_CHECK = async (data) => {
   const toRadians = val => val * Math.PI / 180
   // Calculate a point winthin a circle
   // circle ={center: {latitude: 00.0000, longitude: 00.0000}, radius: number} // in metres
@@ -26,21 +36,15 @@ const LOCATION_CHECK = (data) => {
   // NOTE: First Param is the point you want to check { latitude: 30.0000, longitude: -95.0000 }
   // NOTE Next param is the center of a school building  with your paramas out
   // { center: { latitude: 30.0000, longitude: -95.0000 }, radius: 800 }
-  const HOLLEMAN = pointInsideCircle(data, {center: { latitude: 30.0672, longitude: -95.9239 }, radius: 115})
-  const TECH_OFFICE = pointInsideCircle(data, {center: { latitude: 30.063547, longitude: -95.926245 }, radius: 75})
-  const WALLER_JR_HIGH = pointInsideCircle(data, {center: { latitude: 30.063091, longitude: -95.925915 }, radius: 70})
-  const WALLER_JR_HIGH_2 = pointInsideCircle(data, {center: { latitude: 30.063128, longitude: -95.924069 }, radius: 70})
-  const WALLER_JR_HIGH_3 = pointInsideCircle(data, {center: { latitude: 30.063013, longitude: -95.925098 }, radius: 70})
-  const WALLER_HIGH = pointInsideCircle(data, {center: { latitude: 30.078697, longitude: -95.920345 }, radius: 275})
-  const SHULTZ_JR_HIGH = pointInsideCircle(data, {center: { latitude: 30.057819, longitude: -95.910033 }, radius: 150})
-  const JES = pointInsideCircle(data, {center: { latitude: 30.085986, longitude: -95.978776 }, radius: 175})
-  const RRE = pointInsideCircle(data, {center: { latitude: 30.024168, longitude: -95.813759 }, radius: 175})
-  const TES = pointInsideCircle(data, {center: { latitude: 30.114702, longitude: -95.841073 }, radius: 175})
-  const FSE = pointInsideCircle(data, {center: { latitude: 30.166637, longitude: -95.931027 }, radius: 175})
-
-  const HOLDER = [HOLLEMAN, TECH_OFFICE, WALLER_JR_HIGH, WALLER_JR_HIGH_2, WALLER_JR_HIGH_3, WALLER_HIGH, SHULTZ_JR_HIGH, JES, RRE, TES, FSE]
-  const TEST = HOLDER.includes(true)
+  const TEMP = []
+  const SCHOOLS = await models.Campus.findAll()
+  for (const school in SCHOOLS) {
+    // NOTE: data comes from the function
+    // NOTE: school.... comes from the object in your database
+    TEMP.push(pointInsideCircle(data, { center: { latitude: school.latitude, longitude: school.longitude }, radius: school.radius }))
+  }
+  const TEST = TEMP.includes(true)
   return TEST
 }
 
-console.log(LOCATION_CHECK({ latitude: 30.166889, longitude: -95.931380 }))
+console.log(LOCATION_CHECK({ latitude: 30.062676, longitude: -95.923599 }))
